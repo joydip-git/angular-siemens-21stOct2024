5356e63012d0be2d664778d2fed1028c12e7fa9c
Joydip <joydip.mondal@gmail.com>
2024-10-21 12:57:43 +0530

    basics of JS

---
+++ codes/day-1/js-basics-app/1-data-types.js
@@ -0,0 +1,15 @@
+//variable declaration: using var, const, let keywords
+var numberVariable //=undefined
+numberVariable = 10
+//you can assign som other value also to the same variable
+//numberVariable = 'joydip'
+
+var nameVaraiable = 'joydip'
+var booleanVariable = true
+
+console.log(typeof numberVariable)
+console.log(typeof nameVaraiable)
+console.log(typeof booleanVariable)
+
+
+
