c76da17b38ce0fc28ebf5055c61e105a36a41353
Joydip <joydip.mondal@gmail.com>
2024-10-25 12:51:49 +0530

    routing update

--- codes/day-5/pms-app-v4/src/app/products/components/update-product/update-product.component.ts
+++ codes/day-5/pms-app-v4/src/app/products/components/update-product/update-product.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-product',
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
-export class UpdateProductComponent {
+export class UpdateProductComponent implements OnInit {
 
+  constructor(private currentRoute: ActivatedRoute) {
+
+  }
+  ngOnInit(): void {
+    const id = Number(this.currentRoute.snapshot.params['id'])
+    console.log(id);
+  }
 }
--- codes/day-5/pms-app-v4/src/app/products/components/view-product/view-product.component.ts
+++ codes/day-5/pms-app-v4/src/app/products/components/view-product/view-product.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Params, Router } from '@angular/router';
+import { APP_CONSTANTS } from '../../../config/appconstants';
+import { ServiceContract } from '../../services/abstraction/servicecontract';
+import { Product } from '../../../models/product';
+import { Subscription } from 'rxjs';
+import { ApiResponse } from '../../../models/apiresponse';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
   styleUrl: './view-product.component.css'
 })
-export class ViewProductComponent {
+export class ViewProductComponent implements OnInit, OnDestroy {
+  private getSubscription?: Subscription;
+  productInfo?: Product;
+  fetchComplete = false
+  errorMessage = ''
 
+  constructor(
+    private currentRoute: ActivatedRoute,
+    private router: Router,
+    @Inject(APP_CONSTANTS.PRODUCT_SERVICE_TOKEN)
+    private ps: ServiceContract<Product>
+  ) {
+
+  }
+  goTo(id: number) {
+    this.router.navigate(['/products', 'update', id])
+  }
+  ngOnDestroy(): void {
+    this.getSubscription?.unsubscribe()
+  }
+  ngOnInit(): void {
+    //this.currentRoute.params.subscribe({})
+    const snapshot: ActivatedRouteSnapshot = this.currentRoute.snapshot
+    const params: Params = snapshot.params
+    const id = Number(params['id'])
+    this.fetchProduct(id)
+  }
+  private fetchProduct(id: number) {
+    this.getSubscription =
+      this.ps.get(id)
+        .subscribe(
+          {
+            next: (response: ApiResponse<Product>) => {
+              if (response.data !== null) {
+                this.productInfo = response.data
+                this.errorMessage = ''
+                this.fetchComplete = true
+              } else {
+                this.productInfo = undefined
+                this.errorMessage = response.message
+                this.fetchComplete = true
+              }
+            },
+            error: (e) => {
+              this.productInfo = undefined
+              this.errorMessage = e.message
+              this.fetchComplete = true
+            }
+          }
+        )
+  }
 }
